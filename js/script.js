7c8ac637c50165d3b8007e7c6145e4addcb3e3ab
Ruan Dos Santos <100040124+Ruandossantos@users.noreply.github.com>
2024-10-19 19:59:42 -0300

    11%

--- js/script.js
+++ js/script.js
@@ -32,3 +32,37 @@ function destaqueright (){
     jeep.style.display = "none"
 }
 }
+
+destaqueright();
+
+
+
+
+
+
+
+
+function creta(){
+
+    var creta_frente = document.getElementById("creta-frente");
+    var creta_fundo =  document.getElementById("creta-fundo");
+    var creta_interna = document.getElementById("creta-interna");
+    var creta_lado = document.getElementById("creta-lado");
+
+    var creta = creta_frente.style.display = "block";
+    var creta_f = creta_fundo.style.display = "none";
+    var creta_i = creta_interna.style.display = "none";
+    var creta_l = creta_lado.style.display = "none";
+    
+
+    if (creta === "block"){
+        creta_frente.style.display = "none";
+        creta_fundo.style.display = "block";
+    }else{
+        creta_frente.style.display = "none";
+        creta_fundo.style.display = "none";
+        creta_i = creta_interna.style.display = "none";
+    }
+}
+
+
