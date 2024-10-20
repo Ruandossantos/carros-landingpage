7d2c90b44a8dff10243264f058a52ad7d2b58546
Ruan Dos Santos <100040124+Ruandossantos@users.noreply.github.com>
2024-10-20 15:54:44 -0300

    12%

--- js/script.js
+++ js/script.js
@@ -1,39 +1,3 @@
-function destaqueLeft(){
-    var corola = document.getElementById("corolla");
-    var jeep = document.getElementById("jeep");
-
-   var estadocorola = corola.style.display = "none" 
-   var estadadojeep =jeep.style.display = "block"
-
-    
-   if (estadocorola === "none"){
-        jeep.style.display = "block"
-   }else{
-        corola.style.display = "block" 
-        jeep.style.display = "none"
-   }
-
-}
-
-
-function destaqueright (){
-    var corola = document.getElementById("corolla");
-    var jeep = document.getElementById("jeep");
-
-    
-   var estadocorola = corola.style.display = "none" 
-   var estadadojeep =jeep.style.display = "block"
-
-     
-   if (estadadojeep === "none"){
-    jeep.style.display = "block"
-}else{
-    corola.style.display = "block" 
-    jeep.style.display = "none"
-}
-}
-
-destaqueright();
 
 
 
@@ -42,27 +6,14 @@ destaqueright();
 
 
 
-function creta(){
 
-    var creta_frente = document.getElementById("creta-frente");
-    var creta_fundo =  document.getElementById("creta-fundo");
-    var creta_interna = document.getElementById("creta-interna");
-    var creta_lado = document.getElementById("creta-lado");
 
-    var creta = creta_frente.style.display = "block";
-    var creta_f = creta_fundo.style.display = "none";
-    var creta_i = creta_interna.style.display = "none";
-    var creta_l = creta_lado.style.display = "none";
-    
 
-    if (creta === "block"){
-        creta_frente.style.display = "none";
-        creta_fundo.style.display = "block";
-    }else{
-        creta_frente.style.display = "none";
-        creta_fundo.style.display = "none";
-        creta_i = creta_interna.style.display = "none";
-    }
-}
+
+
+
+
+
+
 
 
